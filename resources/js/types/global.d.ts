802ca255d3bd589ea5ca788521603945e051e19f
fabiorodriguesroque <fabioroque9@gmail.com>
2023-12-09 14:47:33 +0000

    setup laravel echo & clean welcome page

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -2,10 +2,15 @@ import { PageProps as InertiaPageProps } from '@inertiajs/core';
 import { AxiosInstance } from 'axios';
 import ziggyRoute, { Config as ZiggyConfig } from 'ziggy-js';
 import { PageProps as AppPageProps } from './';
+import { Pusher } from 'pusher-js';
+import { Echo } from 'laravel-echo';
 
 declare global {
+    console.log('hello')
     interface Window {
         axios: AxiosInstance;
+        Pusher: Pusher;
+        Echo: Echo;
     }
 
     var route: typeof ziggyRoute;
